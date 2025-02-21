e188de77da461c4ec6ee9400fc9244e7964cbcf1
okino shogo <okisho0106@gmail.com>
2025-02-22 00:38:21 +0900

    Initial Commit

---
+++ TestBunkai/TestBunkaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBunkaiApp.swift
+//  TestBunkai
+//
+//  Created by 沖野匠吾 on 2025/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TestBunkaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
